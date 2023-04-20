fa27a3086cd299bc956174d780594b7ba4112204
pakitowka <whitesaint12@gmail.com>
2023-04-20 20:53:43 +0200

    day19 add to-do

--- web.py
+++ web.py
@@ -3,6 +3,13 @@ import modules.functions as func
 
 todos = func.get_todos()
 
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    func.write_todos(todos)
+
+
 st.title("My to-do app")
 st.subheader("This is my to-do app")
 st.write("This app is to increase your productivity")
@@ -10,4 +17,5 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new to-do...")
+st.text_input(label="", placeholder="Add a new to-do...",
+              on_change=add_todo, key="new_todo")
